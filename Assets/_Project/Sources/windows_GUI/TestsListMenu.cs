f8bf4e64cfab71b439243b2e1c0486b58539e289
nasusfen <ravshannedonat@gmail.com>
2024-06-11 14:56:35 +0300

    GerchikView

--- Assets/_Project/Sources/windows_GUI/TestsListMenu.cs
+++ Assets/_Project/Sources/windows_GUI/TestsListMenu.cs
@@ -8,21 +8,30 @@ using System;
 public class TestsListMenu : Window
 {
     [SerializeField] private Button _voiceTest;
+    [SerializeField] private Button _gerchikTest;
 
-    public event Action VoiceTestButtonClicked; 
+    public event Action VoiceTestButtonClicked;
+    public event Action GerchikTestButtonClicked;
 
     private void OnEnable()
     {
         _voiceTest.onClick.AddListener(OnVoiceTestButtonClick);
+        _gerchikTest.onClick.AddListener(OnGerchikTestButtonClick);
     }
 
     private void OnDisable()
     {
         _voiceTest.onClick.RemoveListener(OnVoiceTestButtonClick);
+        _gerchikTest.onClick.RemoveListener(OnGerchikTestButtonClick);
     }
 
     private void OnVoiceTestButtonClick()
     {
         VoiceTestButtonClicked?.Invoke();
+    }
+
+    private void OnGerchikTestButtonClick()
+    {
+        GerchikTestButtonClicked?.Invoke();
     }
 }
--- Assets/_Project/Sources/windows_GUI/WindowsHandler.cs
+++ Assets/_Project/Sources/windows_GUI/WindowsHandler.cs
@@ -11,6 +11,7 @@ public class WindowsHandler : MonoBehaviour
     [SerializeField] private TestMenu _testAdzies;
     [SerializeField] private ResultMenu _resultAdziesMenu;
     [SerializeField] private VoiceWindow _testVoice;
+    [SerializeField] private Window _testGerchik;
 
     private void Awake()
     {
@@ -29,6 +30,8 @@ public class WindowsHandler : MonoBehaviour
         _resultAdziesMenu.NextWindowButtonClicked += ToMainMenu;
         _testsListWindow.VoiceTestButtonClicked += VoiceCheckMenu;
         _testVoice.MainMenuButtonClicked += ToMainMenu;
+        _testGerchik.NextWindowButtonClicked += ToMainMenu;
+        _testsListWindow.GerchikTestButtonClicked += GerchikButtonClick;
     }
 
     private void OnDisable()
@@ -39,6 +42,8 @@ public class WindowsHandler : MonoBehaviour
         _resultAdziesMenu.NextWindowButtonClicked -= ToMainMenu;
         _testsListWindow.VoiceTestButtonClicked -= VoiceCheckMenu;
         _testVoice.MainMenuButtonClicked -= ToMainMenu;
+        _testGerchik.NextWindowButtonClicked -= ToMainMenu;
+        _testsListWindow.GerchikTestButtonClicked -= GerchikButtonClick;
     }
 
     private void SignInButtonClick()
@@ -51,6 +56,12 @@ public class WindowsHandler : MonoBehaviour
     {
         _testsListWindow.Close();
         _testAdzies.Open();
+    }
+
+    private void GerchikButtonClick()
+    {
+        _testsListWindow.Close();
+        _testGerchik.Open();
     }
 
     private void VoiceCheckMenu()
@@ -66,13 +77,14 @@ public class WindowsHandler : MonoBehaviour
         _resultAdziesMenu.Open();
     }
 
-    private void ToMainMenu()
+    public void ToMainMenu()
     {
         Debug.Log($"To main menu");
         _signInWindow.Close();
         _testAdzies.Close();
         _resultAdziesMenu.Close();
         _testVoice.Close();
+        _testGerchik.Close();
         
         _testsListWindow.Open();
     }
